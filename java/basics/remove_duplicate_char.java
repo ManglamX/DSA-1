c6039cae70bea4b2824fb8ad15624a1007e6e222
41vi4p <dav7.digi@yahoo.com>
2026-01-06 17:44:39 +0530

    java2

---
+++ java/basics/remove_duplicate_char.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class remove_duplicate_char {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String input = sc.nextLine();
+        sc.close();
+        for(int i=0;i<input.length();i++){
+            char ch=input.charAt(i);
+            if(input.indexOf(ch)==i){
+                System.out.print(ch);
+            }
+        }
+        
+    }
+}
---
+++ java/basics/words_in_str.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class words_in_str {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        String[] words = input.split("\\s+");
+        System.out.println("words: " + words.length);
+        sc.close();
+    }
+}
